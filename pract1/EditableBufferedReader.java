ac465a9c0e49e3011991f95bdf3a38dae201ccf2
amfaundez <79316520+amfaundez@users.noreply.github.com>
2021-03-05 12:03:47 +0100

    Update EditableBufferedReader.java

--- pract1/EditableBufferedReader.java
+++ pract1/EditableBufferedReader.java
@@ -1,1 +1,35 @@
+import java.io.*;
 
+
+public class EditableBufferedReader extends BufferedReader {
+
+    public EditableBufferedReader(Reader reader) {
+        super(reader);
+    }
+
+    public void setRaw() {
+        String[] cmd = {"/bin/sh", "-c", "stty raw </dev/tty"};
+        try {
+            Runtime.getRuntime().exec(cmd).waitFor();
+        } catch (Exception e) {
+        }
+    }
+    
+    public void unsetRaw(){
+        String[] cmd = {"/bin/sh", "-c", "stty echo cooked </dev/tty"};
+        try {
+            Runtime.getRuntime().exec(cmd).waitFor();
+        } catch (Exception e) {
+        }
+    }
+      
+    @Override
+    public int read(){
+        return 0; 
+    }
+    
+    @Override
+    public String readLine(){
+        return null;
+    }
+}
