422848c6b45d7cb412456be4830af35a47d063d2
amfaundez <79316520+amfaundez@users.noreply.github.com>
2021-03-17 12:58:15 +0100

    Update EditableBufferedReader.java

--- pract1/EditableBufferedReader.java
+++ pract1/EditableBufferedReader.java
@@ -2,14 +2,14 @@ import java.io.*;
 
 public class EditableBufferedReader extends BufferedReader {
 
-    public static final int HOME= 2000; //o -2^32;
-    public static final int INS= 2001;
-    public static final int DEL=2002;
-    public static final int END= 2003;
-    public static final int RIGHT= 2004;
-    public static final int LEFT= 2005;
-    public static final int UP= 2006;
-    public static final int DOWN= 2007; 
+    public static final int HOME = 2000; //o -2^32;
+    public static final int INS = 2001;
+    public static final int DEL = 2002;
+    public static final int END = 2003;
+    public static final int RIGHT = 2004;
+    public static final int LEFT = 2005;
+    public static final int UP = 2006;
+    public static final int DOWN = 2007;
 
     protected Line line;
 
@@ -25,77 +25,86 @@ public class EditableBufferedReader extends BufferedReader {
         } catch (Exception e) {
         }
     }
-    
-    public void unsetRaw(){
+
+    public void unsetRaw() {
         String[] cmd = {"/bin/sh", "-c", "stty echo cooked </dev/tty"};   //stty echo-> disable echoing of terminal input
         try {
             Runtime.getRuntime().exec(cmd).waitFor();
         } catch (Exception e) {
         }
     }
-      
+
     @Override
     public int read() throws IOException {
-        int reeded, reeded_aux;
-        reeded=super.read();
-        if(reeded!='\033'){ // ESC--> ^[
-            return reeded;
+        int read, read_aux;
+        read = super.read();
+        if (read != '\033') { // ESC--> ^[
+            return read;
         }
-        reeded=super.read();
-        if(reeded=='0'){
-            switch (reeded=super.read()){
-                case 'H': return HOME;
-                case 'F': return END;
-                default: return reeded;
+        read = super.read();
+        if (read == '0') {
+            switch (read = super.read()) {
+                case 'H':
+                    return HOME;
+                case 'F':
+                    return END;
+                default:
+                    return read;
             }
         }
-        if(reeded=='['){
-            switch (reeded=super.read()){
-                case 'A': return UP;
-                case 'B': return DOWN;
-                case 'C': return RIGHT;
-                case 'D': return LEFT;
-                case '1': 
+        if (read == '[') {
+            switch (read = super.read()) {
+                case 'A':
+                    return UP;
+                case 'B':
+                    return DOWN;
+                case 'C':
+                    return RIGHT;
+                case 'D':
+                    return LEFT;
+                case '1':
                 case '2':
                 case '3':
-                case '4': 
-                    if((reeded_aux=super.read())!= '~'){
-                        return reeded_aux; 
+                case '4':
+                    if ((read_aux = super.read()) != '~') {
+                        return read_aux;
                     }
-                    return HOME+reeded-'1';
-                default: return reeded;
+                    return HOME + read - '1';
+                default:
+                    return read;
             }
         }
-        return reeded; 
+        return read;
     }
-    
+
     @Override
-	public String readLine() throws IOException{
-		int readed;
-		setRaw();
-		while((readed=this.read()) != 13){	//ENTER
-			switch (readed) {
-				case LEFT:			
-					//metodo en Line
-					break;
-				case RIGHT: 			
-					//metodo en Line
-					break;
-				case HOME:			
-					//metodo en Line
-				case END:			
-					//metodo en Line
-					break;
-				case INS:
-					//metodo en Line
-					break;	
-				case DEL:		
-                    //metodo en Line
+    public String readLine() throws IOException {
+        int readed;
+        setRaw();
+        while ((readed = this.read()) != 13) {	//ENTER
+            switch (readed) {
+                case LEFT:
+                    line.goLeft();
                     break;
-				default:
-			}
-		}
-		unsetRaw();
+                case RIGHT:
+                    line.goRight();
+                    break;
+                case HOME:
+                    line.goHome();
+                    break;
+                case END:
+                    line.goEnd();
+                    break;
+                case INS:
+                    line.changeMode();
+                    break;
+                case DEL:
+                    line.del(); 
+                    break; //Falta surp y constante
+                default:
+            }
+        }
+        unsetRaw();
         return null;
-	}
+    }
 }
