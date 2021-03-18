6b2f842cbb5aa5e89e68ec917c88bef0b43ec4b6
amfaundez <79316520+amfaundez@users.noreply.github.com>
2021-03-18 17:30:58 +0100

    Update EditableBufferedReader.java

--- pract1/EditableBufferedReader.java
+++ pract1/EditableBufferedReader.java
@@ -2,14 +2,15 @@ import java.io.*;
 
 public class EditableBufferedReader extends BufferedReader {
 
-    public static final int HOME = 2000; //o -2^32;
-    public static final int INS = 2001;
-    public static final int DEL = 2002;
-    public static final int END = 2003;
-    public static final int RIGHT = 2004;
-    public static final int LEFT = 2005;
-    public static final int UP = 2006;
-    public static final int DOWN = 2007;
+    public static final int HOME = -1; //o -2^32;
+    public static final int INS = -2;
+    public static final int DEL = -3;
+    public static final int END = -4;
+    public static final int SUPR = -5;
+    public static final int RIGHT = -6;
+    public static final int LEFT = -7;
+    public static final int UP = -8;
+    public static final int DOWN = -9;
 
     protected Line line;
 
@@ -41,40 +42,40 @@ public class EditableBufferedReader extends BufferedReader {
         if (read != '\033') { // ESC--> ^[
             return read;
         }
-        read = super.read();
-        if (read == '0') {
-            switch (read = super.read()) {
-                case 'H':
-                    return HOME;
-                case 'F':
-                    return END;
-                default:
-                    return read;
-            }
+        switch(read = super.read()){
+            case '0':
+                switch (read = super.read()) {
+                    case 'H':
+                        return HOME;
+                    case 'F':
+                        return END;
+                    default:
+                        return read;
+                }
+            case '[':
+                switch (read = super.read()) {
+                    case 'A':
+                        return UP;
+                    case 'B':
+                        return DOWN;
+                    case 'C':
+                        return RIGHT;
+                    case 'D':
+                        return LEFT;
+                    case '1':
+                    case '2':
+                    case '3':
+                    case '4':
+                        if ((read_aux = super.read()) != '~') {
+                            return read_aux;
+                        }
+                        return HOME + read - '1';
+                    default:
+                        return read;
+                }
+            default:
+                return read;
         }
-        if (read == '[') {
-            switch (read = super.read()) {
-                case 'A':
-                    return UP;
-                case 'B':
-                    return DOWN;
-                case 'C':
-                    return RIGHT;
-                case 'D':
-                    return LEFT;
-                case '1':
-                case '2':
-                case '3':
-                case '4':
-                    if ((read_aux = super.read()) != '~') {
-                        return read_aux;
-                    }
-                    return HOME + read - '1';
-                default:
-                    return read;
-            }
-        }
-        return read;
     }
 
     @Override
@@ -85,22 +86,31 @@ public class EditableBufferedReader extends BufferedReader {
             switch (readed) {
                 case LEFT:
                     line.goLeft();
+                    System.out.print("\033[D");
                     break;
                 case RIGHT:
                     line.goRight();
+                    System.out.print("\033[C");
                     break;
                 case HOME:
                     line.goHome();
+                    System.out.print("\033[H");
                     break;
                 case END:
                     line.goEnd();
+                    System.out.print("\033[F");
                     break;
                 case INS:
                     line.changeMode();
                     break;
                 case DEL:
                     line.del(); 
-                    break; //Falta surp y constante
+                    System.out.print("\033[P");
+                    break;
+                case SUPR:
+                    line.bksp();
+                    System.out.print("\033[D");
+                    break;
                 default:
             }
         }
