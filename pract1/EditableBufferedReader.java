8cc54db23a242fcf816dbd49ea7f50553710fe82
amfaundez <79316520+amfaundez@users.noreply.github.com>
2021-03-22 17:12:29 +0100

    Update EditableBufferedReader.java

--- pract1/EditableBufferedReader.java
+++ pract1/EditableBufferedReader.java
@@ -2,11 +2,10 @@ import java.io.*;
 
 public class EditableBufferedReader extends BufferedReader {
 
-    public static final int HOME = -1; //o -2^32;
+    public static final int HOME = -1; // o -2^32;
     public static final int INS = -2;
     public static final int DEL = -3;
     public static final int END = -4;
-    public static final int SUPR = -5;
     public static final int RIGHT = -6;
     public static final int LEFT = -7;
     public static final int UP = -8;
@@ -19,16 +18,20 @@ public class EditableBufferedReader extends BufferedReader {
         line = new Line();
     }
 
+
     public void setRaw() {
-        String[] cmd = {"/bin/sh", "-c", "stty raw </dev/tty"};
+        String[] cmd = { "/bin/sh", "-c", "stty -echo raw </dev/tty" };
+        //String[] cmd = {"/bin/sh", "-c", "echo -ne '\033[6n'"};
         try {
             Runtime.getRuntime().exec(cmd).waitFor();
         } catch (Exception e) {
         }
     }
 
+
     public void unsetRaw() {
-        String[] cmd = {"/bin/sh", "-c", "stty echo cooked </dev/tty"};   //stty echo-> disable echoing of terminal input
+        String[] cmd = { "/bin/sh", "-c", "stty echo cooked </dev/tty" }; // stty echo-> disable echoing of terminal
+                                                                          // input
         try {
             Runtime.getRuntime().exec(cmd).waitFor();
         } catch (Exception e) {
@@ -39,21 +42,41 @@ public class EditableBufferedReader extends BufferedReader {
     public int read() throws IOException {
         int read, read_aux;
         read = super.read();
-        if (read != '\033') { // ESC--> ^[
+        if (read != '\033')// ESC--> ^[
             return read;
-        }
-        switch(read = super.read()){
-            case '0':
+        /*
+            switch (read = super.read()) { 
+                case '0': 
+                    switch (read = super.read()) { 
+                        case 'H': return HOME; 
+                        case 'F': return END; 
+                        default: return read; 
+                    } 
+                case '[':
+                    switch (read = super.read()) { 
+                        case 'A': return UP; 
+                        case 'B': return DOWN;
+                        case 'C': return RIGHT; 
+                        case 'D': return LEFT; 
+                        case '1': 
+                        case '2': 
+                        case '3':
+                        case '4': 
+                            if ((read_aux = super.read()) != '~') 
+                                return read_aux; 
+                            return HOME - read + '1'; 
+                        default: return read; 
+                    } 
+                default: return read; 
+            }
+         */
+        switch (read = super.read()) {
+            case '[':
                 switch (read = super.read()) {
                     case 'H':
                         return HOME;
                     case 'F':
                         return END;
-                    default:
-                        return read;
-                }
-            case '[':
-                switch (read = super.read()) {
                     case 'A':
                         return UP;
                     case 'B':
@@ -69,7 +92,9 @@ public class EditableBufferedReader extends BufferedReader {
                         if ((read_aux = super.read()) != '~') {
                             return read_aux;
                         }
-                        return HOME + read - '1';
+                        // return HOME + read - '1';
+                        return HOME - read + '1';
+
                     default:
                         return read;
                 }
@@ -80,41 +105,58 @@ public class EditableBufferedReader extends BufferedReader {
 
     @Override
     public String readLine() throws IOException {
-        int readed;
-        setRaw();
-        while ((readed = this.read()) != 13) {	//ENTER
-            switch (readed) {
-                case LEFT:
-                    line.goLeft();
-                    System.out.print("\033[D");
-                    break;
-                case RIGHT:
-                    line.goRight();
-                    System.out.print("\033[C");
-                    break;
-                case HOME:
-                    line.goHome();
-                    System.out.print("\033[H");
-                    break;
-                case END:
-                    line.goEnd();
-                    System.out.print("\033[F");
-                    break;
-                case INS:
-                    line.changeMode();
-                    break;
-                case DEL:
-                    line.del(); 
-                    System.out.print("\033[P");
-                    break;
-                case SUPR:
-                    line.bksp();
-                    System.out.print("\033[D");
-                    break;
-                default:
+        int read;
+        try{
+            setRaw();
+            System.out.print("\033[H\033[J"); //FORMAT TERMINAL
+		    System.out.flush();
+
+            while ((read = this.read()) != 13) { // ENTER
+                switch (read) {
+                    case LEFT:
+                        line.goLeft();
+                        System.out.print("\033[D");
+                        break;
+                    case RIGHT:
+                        line.goRight();
+                        System.out.print("\033[C");
+                        break;
+                    case HOME:
+                        line.goHome();
+                        //System.out.print("\033[3;0H"); sin el format terminal
+                        System.out.print("\033[H");
+                        break;
+                    case END:
+                        line.goEnd();
+                        System.out.print("\033[0;" + (line.getCursor() + 1) + "H");
+                        break;
+                    case INS:
+                        line.changeMode();
+                        //System.out.print("\033[2~");
+                        break;
+                    case DEL:
+                        line.del();
+                        //System.out.print("\033[3~");
+                        System.out.print("\033[P"); //Delete character
+                        break;
+                    case 127: //BACKSPACE
+                        line.bksp();
+                        System.out.print("\033[D\033[P"); 
+                        //System.out.print("\033[P"); 
+                        break;
+                    default:
+                        line.write((char) read);
+                        if (!line.modeIns){
+                            System.out.print("\033[@"); //Insert blank character
+                        }
+                        System.out.print((char) read);
+                }
             }
+        }catch (Exception e) {
+            throw e;
+        }finally{
+            unsetRaw();
         }
-        unsetRaw();
-        return null;
+        return line.toString();
     }
 }
