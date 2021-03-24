86e2d572610fcb291923ba953b58c754a8e2285e
amfaundez <79316520+amfaundez@users.noreply.github.com>
2021-03-24 16:24:49 +0100

    Create TestReadLineMVC.java

---
+++ pract1/with MVC/Property Change/TestReadLineMVC.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class TestReadLineMVC {
+	public static void main(String[] args) {
+		BufferedReader in = new EditableBufferedReaderMVC(new InputStreamReader(System.in));
+		String str = null;
+		try {
+			str = in.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("\nline is: " + str);
+	}
+}
