f08748745292e44ffc3c71a8506217d9a0bff81a
amfaundez <79316520+amfaundez@users.noreply.github.com>
2021-06-16 12:52:51 +0200

    Update TestReadLine.java

--- pract1/without MVC/TestReadLine.java
+++ pract1/without MVC/TestReadLine.java
@@ -3,7 +3,6 @@ import java.io.*;
 public class TestReadLine {
 	public static void main(String[] args) {
 		BufferedReader in = new EditableBufferedReader(new InputStreamReader(System.in));
-		//BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		String str = null;
 		try {
 			str = in.readLine();
